fcc83b2909e9e01918f559c11b3a7dad896e31c6
Sangee Yoon <sk41442457@gmail.com>
2026-02-02 10:36:24 +0900

    Initial commit

---
+++ src/test/java/com/example/imprint/ImprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
